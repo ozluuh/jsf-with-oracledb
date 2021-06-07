d550ae7df934ffbffe9afa9fe52af103d134cf07
ozluuh <ozluuh@gmail.com>
2021-06-07 18:20:07 -0300

    add persistence logic in bean

--- src/main/java/com/test/bean/IndexBean.java
+++ src/main/java/com/test/bean/IndexBean.java
@@ -1,16 +1,30 @@
 package com.test.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
 
+import com.test.dao.ConnectionFactory;
+import com.test.dao.Persisted;
 import com.test.model.Customer;
 
 @ManagedBean
 public class IndexBean {
 
+    private static final EntityManager em = ConnectionFactory.getInstance().createEntityManager();
+
     private Customer customer = new Customer();
 
+    private Persisted<Customer, Long> dao = new Persisted<Customer, Long>(em);
+
     public void execute() {
-        System.out.println("\n\n\nStored: " + this.customer);
+        dao.save(customer);
+
+        try {
+            dao.commit();
+            System.out.println("\n\n\nStored: " + this.customer);
+        } catch (Exception e) {
+            System.out.println("\n\n\n" + e.getMessage());
+        }
     }
 
     public Customer getCustomer() {
