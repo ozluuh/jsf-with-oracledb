04c76a1c3f1e9b7b4cae054a6fa425380fe6b767
ozluuh <ozluuh@gmail.com>
2021-06-07 18:17:57 -0300

    add sequence in model

--- src/main/java/com/test/model/Customer.java
+++ src/main/java/com/test/model/Customer.java
@@ -1,14 +1,19 @@
 package com.test.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="T_CUSTOMER_SAMPLE")
+@Table(name = "T_CUSTOMER_SAMPLE")
+@SequenceGenerator(name = "customer", allocationSize = 1, sequenceName = "SQ_CUSTOMER_SAMPLE")
 public class Customer {
 
     @Id
+    @GeneratedValue(generator = "customer", strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
